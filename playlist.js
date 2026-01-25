09bf890141052fad2d4f4865c33be9801ad37eb6
Dulaksha Chathura <hadulakshachathura@gmail.com>
2026-01-25 19:50:39 +0530

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -27,15 +27,34 @@ function displayVideos(videos) {
   const container = document.getElementById('playlist');
   container.innerHTML = '';
 
-  videos.forEach(video => {
+  // Create the player container at the top
+  const playerDiv = document.createElement('div');
+  playerDiv.id = 'video-player';
+  playerDiv.style.width = '80%';
+  playerDiv.style.maxWidth = '800px';
+  playerDiv.style.height = '450px';
+  playerDiv.style.margin = '0 auto 40px';
+  container.appendChild(playerDiv);
+
+  // Create video cards
+  videos.forEach((video, index) => {
     const videoId = video.snippet.resourceId.videoId;
     const title = video.snippet.title;
     const thumbnail = video.snippet.thumbnails.medium.url;
 
     const card = document.createElement('div');
     card.className = 'video-card';
+
     card.onclick = () => {
-      window.open(`https://www.youtube.com/watch?v=${videoId}`, '_blank');
+      // Load video in iframe
+      playerDiv.innerHTML = `
+        <iframe width="100%" height="100%" 
+          src="https://www.youtube.com/embed/${videoId}?autoplay=1" 
+          title="${title}" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen>
+        </iframe>`;
     };
 
     card.innerHTML = `
@@ -46,6 +65,9 @@ function displayVideos(videos) {
     `;
 
     container.appendChild(card);
+
+    // Auto-click first video to play it on load
+    if (index === 0) card.click();
   });
 }
 
